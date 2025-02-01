d75b53932c92f2ca5beb89cffd067d14c3215123
leyowi <anthonyleouiepalmera310@gmail.com>
2025-02-02 04:07:34 +0800

    Updated the script to tabulate the result

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # This script programs what is your GWA
-print("INSTRUCTION: Enter first your subject then your grades and its respective units.")
-print()
-number = int(input("How many is your subjects / courses? : "))
+number = int(input("How many subjects/courses do you have? : "))
 grade_lists = []
 unit_lists = []
+subject_names = []
 x = 0
 y = 0
 
 for i in range(number):
-    subject_name = str(input("Subject name / code: "))
-    grades_input = float(input("Grade of this subject : "))
-    units_input = float(input("Unit/s of this subject: "))
+    subject_name = str(input("Course: "))
+    units_input = float(input("Unit/s: "))
+    grades_input = float(input("Grade: "))
     print()
     y += units_input
     grade_lists.append(grades_input)
     unit_lists.append(units_input)
+    subject_names.append(subject_name)
     grades_input *= units_input
     x += grades_input
 
@@ -22,32 +22,14 @@ z = x / y
 print()
 print("GWA: ", z)
 
-if z <= 1.0:
-    print("= 97-100")
-    print("Excellent")
-elif z <= 1.25:
-    print("= 94-96")
-    print("Excellent")
-elif z <= 1.5:
-    print("= 91-93")
-    print("Very Good")
-elif z <= 1.75:
-    print("= 88-90")
-    print("Very Good")
-elif z <= 2.0:
-    print("= 85-87")
-    print("Good")
-elif z <= 2.25:
-    print("= 82-84")
-    print("Good")
-elif z <= 2.5:
-    print("= 79-81")
-    print("Satisfactory")
-elif z <= 2.75:
-    print("= 76-78")
-    print("Satisfactory")
-elif z <= 3.0:
-    print("= 75")
-    print("Passed")
+if z < 1.5:
+    print("President's Lister")
 else:
-    print("Failed")
+    print("Dean's Lister")
+
+# Tabulate the results
+print("\n{:<20} {:<10} {:<10}".format("Course", "Units", "Grades"))
+print("-" * 40)
+for i in range(number):
+    print("{:<20} {:<10} {:<10}".format(subject_names[i], unit_lists[i], grade_lists[i]))
+print("-" * 40)
